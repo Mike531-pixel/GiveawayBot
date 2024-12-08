b2aa74277e9d638ac388297d76f6c2fed042647f
Itz Random <100831398+ItzRandom23@users.noreply.github.com>
2024-12-08 13:57:01 +0530

    Update invite.js

--- src/commands/Information/invite.js
+++ src/commands/Information/invite.js
@@ -4,7 +4,7 @@ module.exports = {
 
   run: async ({ client, context }) => {
     return await context.createMessage({
-      content: `<:yay:1310201036732104746> Hello! I'm **GiveawayBot**! I help to make giveaways quick and easy!\nYou can add me to your server with this link:\n\n🔗 **https://discord.com/oauth2/authorize?permissions=347200&scope=bot+applications.commands&client_id=${client.user.id}**`,
+      content: `<:yay:1310201036732104746> Hello! I'm **GiveawayBot**! I help to make giveaways quick and easy!\nYou can add me to your server with this link:\n\n🔗 **https://discord.com/oauth2/authorize?permissions=354368&scope=bot+applications.commands&client_id=${client.user.id}**`,
     });
   },
 };
