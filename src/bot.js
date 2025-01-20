50bbb92eb393984ff6cf2bdd4d50daf4d36cce3a
Itz Random <100831398+ItzRandom23@users.noreply.github.com>
2025-01-20 22:38:22 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,1 +1,19 @@
+const { EmbedBuilder } = require("discord.js");
+const { inspect } = require('util');
 new (require("./structures/Client"))().build();
+
+process.removeAllListeners("warning");
+
+process.on("unhandledRejection", async (err, promise) => {
+  if (!String(err).includes("Unknown interaction") && !String(err).includes("Missing Access") && !String(err).includes("Missing Permissions") && !String(err).includes("Unknown Message") && !String(err).includes("InteractionAlreadyReplied") && !String(err).includes("Unknown Member")) {
+    console.log("Unhandled Rejection:", err);
+    console.log("Promise:", promise);
+  }
+});
+
+process.on("uncaughtException", async (err, promise) => {
+  if (!String(err).includes("Unknown interaction") && !String(err).includes("Missing Access") && !String(err).includes("Missing Permissions") && !String(err).includes("Unknown Message") && !String(err).includes("InteractionAlreadyReplied") && !String(err).includes("Unknown Member")) {
+    console.log("Uncaught Exception:", err);
+    console.log("Promise:", promise);
+  }
+});
