031a34c2999e505ab56f91599cfe6a5b00a2f75d
Mike531-pixel <Guerbensseide@gmail.com>
2025-06-23 11:29:34 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "",
-  MongoDB: "",
-  dev: "",
+  TOKEN: "MTM4NjcyOTI3MDQyODA0NTM4NQ.GY0DGe.zoJmw38Na_ifXr1KeGIgN1Yyt7aF-VnoI9f6Vo",
+  MongoDB: "mongodb+srv://public:public6969@cluster0.hcetn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  dev: "868950412630306886",
 };
